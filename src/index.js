c91115e602de6428bd79ad93faeff3b89d9f074b
Gbemiga Meduoye <gbemiga@claymoreminds.com>
2018-07-19 16:31:51 +0100

    Added strategies

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import DaoMiddleware from './DaoMiddleware';
 export * from './dao'
 export * from './dataSinks'
 export * from './rx'
+export * from './subscriptionStrategies'
 export {default as DaoMiddleware} from './DaoMiddleware'
 export {default as DaoReducer} from './DaoReducer'
---
+++ src/subscriptionStrategies/OperatorSubscriptionStrategy.js
@@ -0,0 +1,25 @@
+import {debounce} from 'lodash';
+export default class OperatorSubscriptionStrategyStrategy{
+  constructor(client, path, dataSink){
+    this.client = client;
+    this.dataSink = dataSink;
+    this.path = path;
+    this.dispose = this.dispose.bind(this);
+    this.updateSubscription = this.updateSubscription.bind(this);
+  }
+
+  async updateSubscription(options){
+    if (this.subscribeCommand === undefined){
+      this.subscribeCommand = this.client.subscribe(this.path, options, this.dataSink);
+    } else {
+      this.client.updateSubscription(this.subscribeCommand.id, options, this.dataSink);
+    }
+    return this.dataSink.dataSinkUpdated.waitForSuccess().toPromise();
+  }
+
+  dispose(){
+    if (this.subscribeCommand && this.dataSink){
+      this.client.unsubscribe(this.subscribeCommand.id, this.dataSink);
+    }
+  }
+}
