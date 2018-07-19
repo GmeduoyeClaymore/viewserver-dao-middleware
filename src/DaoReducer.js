d1c5576e011bfdb5b5693149ff2dcf6dd4295768
Gbemiga Meduoye <gbemiga@claymoreminds.com>
2018-07-19 16:01:57 +0100

    Fixed another issue

--- src/DaoReducer.js
+++ src/DaoReducer.js
@@ -1,5 +1,5 @@
 import {combineReducers} from 'redux-seamless-immutable';
-import {UPDATE_STATE, UPDATE_COMMAND_STATUS, UPDATE_OPTIONS, RESET_ALL_COMPONENT_STATE, UPDATE_COMPONENT_STATE, CLEAR_COMPONENT_STATE} from './common/dao/ActionConstants';
+import {UPDATE_STATE, UPDATE_COMMAND_STATUS, UPDATE_OPTIONS, RESET_ALL_COMPONENT_STATE, UPDATE_COMPONENT_STATE, CLEAR_COMPONENT_STATE} from './dao/ActionConstants';
 
 const dao = (state = {}, action) => {
   if (action.type && (action.type.startsWith(UPDATE_STATE('')) || action.type.startsWith(UPDATE_OPTIONS('')) || action.type.startsWith(UPDATE_COMMAND_STATUS('')))){
