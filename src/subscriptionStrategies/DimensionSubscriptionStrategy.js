a4b8558d45a1a1002afb80421798bb803584d2cd
Gbemiga Meduoye <gbemiga@claymoreminds.com>
2018-07-20 17:41:52 +0100

    Foo

---
+++ src/subscriptionStrategies/DimensionSubscriptionStrategy.js
@@ -0,0 +1,34 @@
+import {debounce} from 'lodash';
+export default class DimensionSubscriptionStrategy{
+  constructor(client, reportContext, dataSink, dimension){
+    this.client = client;
+    this.dataSink = dataSink;
+    this.dimension = dimension;
+    if (!this.dataSink){
+      throw new Error('Data sink must be specified');
+    }
+    if (!this.dimension){
+      throw new Error('dimension must be specified');
+    }
+    this.reportContext = reportContext;
+    if (!this.reportContext){
+      throw new Error('report context must be specified');
+    }
+    this.dispose = this.dispose.bind(this);
+    this.updateSubscription = debounce(this.updateSubscription.bind(this), 50);
+  }
+
+  updateSubscription(options){
+    if (this.subscribeCommand === undefined){
+      this.subscribeCommand = this.client.subscribeToDimension(this.dimension, this.reportContext, options, this.dataSink);
+    } else {
+      this.client.updateSubscription(this.subscribeCommand.id, options, this.dataSink);
+    }
+  }
+
+  dispose(){
+    if (this.subscribeCommand && this.dataSink){
+      this.client.unsubscribe(this.subscribeCommand.id, this.dataSink);
+    }
+  }
+}
