e0302671b2b1f296d980d67de9d077abbfc3152f
Gbemiga Meduoye <gbemiga@claymoreminds.com>
2018-09-03 13:39:31 +0100

    Now data errors are permitted

--- src/rx/utils.js
+++ src/rx/utils.js
@@ -3,16 +3,13 @@ import RxDataSink from '../dataSinks/RxDataSink';
 
 export const ERROR = 'Error';
 export const SUCCESS = 'Success';
-const DOMAIN_EVENT_TYPES = [RxDataSink.ROW_ADDED, RxDataSink.ROW_UPDATED, RxDataSink.ROW_REMOVED, RxDataSink.DATA_RESET, RxDataSink.SNAPSHOT_COMPLETE,ERROR, SUCCESS ];
+const DOMAIN_EVENT_TYPES = [RxDataSink.DATA_ERROR, RxDataSink.DATA_ERROR_CLEARED, RxDataSink.ROW_ADDED, RxDataSink.ROW_UPDATED, RxDataSink.ROW_REMOVED, RxDataSink.DATA_RESET, RxDataSink.SNAPSHOT_COMPLETE,ERROR, SUCCESS ];
 
 Rx.Observable.prototype.timeoutWithError = function (timeout, error) {
   return this.timeoutWith(timeout, Rx.Observable.throw(error));
 };
 
 const checkForErrorEventTypes = (ev) => {
-  if (ev.Type ===  RxDataSink.DATA_ERROR){
-    return Rx.Observable.throw(new Error('Subscription failed as there is a data error on the server side'));
-  }
   if (ev.Type ===  RxDataSink.SCHEMA_ERROR){
     return Rx.Observable.throw(new Error('Subscription failed as there is a schema error on the server side'));
   }
@@ -23,7 +20,7 @@ const checkForErrorEventTypes = (ev) => {
 };
 
 Rx.Observable.prototype.waitForSnapshotComplete = function (timeout = 10000) {
-  return this.filter(ev => !!~[RxDataSink.SNAPSHOT_COMPLETE, RxDataSink.DATA_ERROR, RxDataSink.SCHEMA_ERROR,  RxDataSink.CONFIG_ERROR].indexOf(ev.Type))
+  return this.filter(ev => !!~[RxDataSink.SNAPSHOT_COMPLETE, RxDataSink.SCHEMA_ERROR,  RxDataSink.CONFIG_ERROR].indexOf(ev.Type))
     .take(1)
     .flatMap(c => checkForErrorEventTypes(c))
     .timeoutWithError(timeout, new Error(`No snapshot complete event detected ${timeout} millis seconds after update`));
