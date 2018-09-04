396c67292ea0d99c2c46b0a7a4aeac4407c7ec5e
Gbemiga Meduoye <gbemiga@claymoreminds.com>
2018-09-04 06:40:37 +0100

    Snapshot complete can now be data error

--- src/dao/DaoBase.js
+++ src/dao/DaoBase.js
@@ -12,6 +12,7 @@ export default class Dao {
     this.rawDataSubject = new Rx.Subject();
     this.optionsSubject = new Rx.Subject();
     this.countSubject = new Rx.Subject();
+    this.dataRequested = new Rx.Subject();
     this.snapshotCompleteSubject = new Rx.Subject();
     this.options = this.daoContext.defaultOptions;
     if (this.daoContext.extendDao){
@@ -84,93 +85,101 @@ export default class Dao {
   }
 
   async updateSubscription(options, force){
-    const newOptions = {...this.options, ...options};
-    if (this.daoContext.doesDataSinkNeedToBeCleared && this.daoContext.doesDataSinkNeedToBeCleared(this.options, newOptions)) {
-      if (this.dataSink){
-        this.dataSink.onDataReset();
-      }
-    }
 
-    if (this.daoContext.doesSubscriptionNeedToBeRecreated(this.options, newOptions) || !this.subscriptionStrategy || force || this.forceNexUpdate){
-      if (this.dataSink){
-        this.dataSink.onDataReset();
-      }
-      if (this.subscriptionStrategy){
-        Logger.info(`Disposing of subscription - ${this.daoContext.name}`);
-        this.subscriptionStrategy.dispose();
-      }
-      if (this.rowEventSubscription){
-        Logger.info(`Disposing of row event subscription - ${this.daoContext.name}`);
-        this.rowEventSubscription.unsubscribe();
-      }
-      if (this.rawDataEventSubscription){
-        this.rawDataEventSubscription.unsubscribe();
-      }
-      if (this.countSubscription){
-        Logger.info(`Disposing of row count subscription - ${this.daoContext.name}`);
-        this.countSubscription.unsubscribe();
-      }
-      if (this.snapshotCompleteSubscription){
-        Logger.info(`Disposing of snapshotcomplete subscription - ${this.daoContext.name}`);
-        this.snapshotCompleteSubscription.unsubscribe();
-      }
-      
-      this.dataSink = this.daoContext.createDataSink(newOptions);
-      this.dataSink.name = this.daoContext.name;
-      this.subscriptionStrategy = await this.daoContext.createSubscriptionStrategy(newOptions, this.dataSink);
+    try{
+      this.dataRequested.next(true);
 
-      this.rowEventObservable = this.dataSink.dataSinkUpdated.filterRowEvents();
-      this.countSubscription = this.dataSink.dataSinkUpdated.filter(ev => ev.Type === RxDataSink.TOTAL_ROW_COUNT).subscribe(ev => this.countSubject.next(ev.count));
-      this.snapshotCompleteSubscription = this.dataSink.dataSinkUpdated.filter(ev => !!~[RxDataSink.DATA_RESET, RxDataSink.SNAPSHOT_COMPLETE].indexOf(ev.Type)).subscribe(() => this.snapshotCompleteSubject.next(this.dataSink.isSnapshotComplete));
-
-      this.rowEventSubscription =  (this.daoContext.adapt ?  this.daoContext.adapt(this.rowEventObservable) : this.rowEventObservable).subscribe((ev) => {
-        if (this.dataSink.isSnapshotComplete || ev.Type == RxDataSink.DATA_RESET) {
-          this.subject.next(this.daoContext.mapDomainEvent(this.dataSink, ev));
-        }
-      });
-      this.rawDataEventSubscription = this.dataSink.dataSinkUpdated.subscribe(ev => this.rawDataSubject.next(ev));
-      Logger.info(`Updating subscription for  ${this.daoContext.name}`);
-    }
-
-    try {
-      if (isEqual(this.options, newOptions) && this.subscribed && !force && !this.forceNexUpdate){
-        return Promise.resolve();
-      }
-      if (this.snapshotSubscription){
-        Logger.info(`!!!!!Found in flight subscription to snapshot complete cancelling !!!! ${this.daoContext.name}`);
-        this.snapshotSubscription.unsubscribe();
-        this.snapshotSubscription = undefined;
-        if (this.promiseReject){
-          this.promiseReject('Operation cancelled by another subscription operation');
-          this.promiseReject = undefined;
+      const newOptions = {...this.options, ...options};
+      if (this.daoContext.doesDataSinkNeedToBeCleared && this.daoContext.doesDataSinkNeedToBeCleared(this.options, newOptions)) {
+        if (this.dataSink){
+          this.dataSink.onDataReset();
         }
       }
-      this.options = newOptions;
-      Logger.info(`Updating options to ${JSON.stringify(this.options)} ${this.daoContext.name}`);
-      this.optionsSubject.next(this.options);
-      const optionsMessage = this.daoContext.transformOptions(this.options);
-      const snapshotObservable = this.dataSink.dataSinkUpdated.waitForSnapshotComplete(10000);
-      const _this = this;
-      const result = new Promise((resolve, reject) => {
-        _this.promiseReject = resolve;
-        _this.snapshotSubscription = snapshotObservable.subscribe(
-          ev => {
-            Logger.info(`!!!!!Completed snapshot complete!!!! ${this.daoContext.name}`);
-            resolve(ev);
-            this.subscribed = true;
-            this.snapshotSubscription.unsubscribe();
-            this.snapshotSubscription = undefined;
-          },
-          err => reject(err)
-        );
-      });
-      this.forceNexUpdate = false;
-      this.subscriptionStrategy.updateSubscription(optionsMessage);
-      Logger.info(`!!!!!Waiting for snapshot complete!!!! ${this.daoContext.name}`);
-      return result;
-    } catch (error){
-      Logger.warning(`!!!!!Error in subscription update !!!! ${error} ${this.daoContext.name}`);
-      return Promise.reject(error);
+
+      if (this.daoContext.doesSubscriptionNeedToBeRecreated(this.options, newOptions) || !this.subscriptionStrategy || force || this.forceNexUpdate){
+        if (this.dataSink){
+          this.dataSink.onDataReset();
+        }
+        if (this.subscriptionStrategy){
+          Logger.info(`Disposing of subscription - ${this.daoContext.name}`);
+          this.subscriptionStrategy.dispose();
+        }
+        if (this.rowEventSubscription){
+          Logger.info(`Disposing of row event subscription - ${this.daoContext.name}`);
+          this.rowEventSubscription.unsubscribe();
+        }
+        if (this.rawDataEventSubscription){
+          this.rawDataEventSubscription.unsubscribe();
+        }
+        if (this.countSubscription){
+          Logger.info(`Disposing of row count subscription - ${this.daoContext.name}`);
+          this.countSubscription.unsubscribe();
+        }
+        if (this.snapshotCompleteSubscription){
+          Logger.info(`Disposing of snapshotcomplete subscription - ${this.daoContext.name}`);
+          this.snapshotCompleteSubscription.unsubscribe();
+        }
+        
+        this.dataSink = this.daoContext.createDataSink(newOptions);
+        this.dataSink.name = this.daoContext.name;
+        this.subscriptionStrategy = await this.daoContext.createSubscriptionStrategy(newOptions, this.dataSink);
+
+        this.rowEventObservable = this.dataSink.dataSinkUpdated.filterRowEvents();
+        this.countSubscription = this.dataSink.dataSinkUpdated.filter(ev => ev.Type === RxDataSink.TOTAL_ROW_COUNT).subscribe(ev => this.countSubject.next(ev.count));
+        this.snapshotCompleteSubscription = this.dataSink.dataSinkUpdated.filter(ev => !!~[RxDataSink.DATA_RESET, RxDataSink.SNAPSHOT_COMPLETE].indexOf(ev.Type)).subscribe(() => this.snapshotCompleteSubject.next(this.dataSink.isSnapshotComplete));
+
+        this.rowEventSubscription =  (this.daoContext.adapt ?  this.daoContext.adapt(this.rowEventObservable) : this.rowEventObservable).subscribe((ev) => {
+          if (this.dataSink.isSnapshotComplete || ev.Type == RxDataSink.DATA_RESET) {
+            this.subject.next(this.daoContext.mapDomainEvent(this.dataSink, ev));
+          }
+        });
+        this.rawDataEventSubscription = this.dataSink.dataSinkUpdated.subscribe(ev => this.rawDataSubject.next(ev));
+        Logger.info(`Updating subscription for  ${this.daoContext.name}`);
+      }
+
+      try {
+        if (isEqual(this.options, newOptions) && this.subscribed && !force && !this.forceNexUpdate){
+          return Promise.resolve();
+        }
+        if (this.snapshotSubscription){
+          Logger.info(`!!!!!Found in flight subscription to snapshot complete cancelling !!!! ${this.daoContext.name}`);
+          this.snapshotSubscription.unsubscribe();
+          this.snapshotSubscription = undefined;
+          if (this.promiseReject){
+            this.promiseReject('Operation cancelled by another subscription operation');
+            this.promiseReject = undefined;
+          }
+        }
+        this.options = newOptions;
+        Logger.info(`Updating options to ${JSON.stringify(this.options)} ${this.daoContext.name}`);
+        this.optionsSubject.next(this.options);
+        const optionsMessage = this.daoContext.transformOptions(this.options);
+        const snapshotObservable = this.dataSink.dataSinkUpdated.waitForSnapshotComplete(10000);
+        const _this = this;
+        const result = new Promise((resolve, reject) => {
+          _this.promiseReject = resolve;
+          _this.snapshotSubscription = snapshotObservable.subscribe(
+            ev => {
+              Logger.info(`!!!!!Completed snapshot complete!!!! ${this.daoContext.name}`);
+              resolve(ev);
+              this.subscribed = true;
+              this.snapshotSubscription.unsubscribe();
+              this.snapshotSubscription = undefined;
+            },
+            err => reject(err)
+          );
+        });
+        this.forceNexUpdate = false;
+        this.subscriptionStrategy.updateSubscription(optionsMessage);
+        Logger.info(`!!!!!Waiting for snapshot complete!!!! ${this.daoContext.name}`);
+        return result;
+      } catch (error){
+        Logger.warning(`!!!!!Error in subscription update !!!! ${error} ${this.daoContext.name}`);
+        return Promise.reject(error);
+      }
+    }
+    finally{
+      this.dataRequested.next(false);
     }
   }
 }
